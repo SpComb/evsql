2b3edbdf89a49992ff9d6e534b18af9e0368fa78
Tero Marttila <terom@fixme.fi>
2009-03-08 01:16:54 +0200

    move to CMake from old crufty Makefile

---
+++ cmake/Modules/FindLibEvent.cmake
@@ -0,0 +1,27 @@
+# Find libevent
+# Once done, this will define:
+#
+#   LibEvent_FOUND
+#   LibEvent_INCLUDE_DIRS
+#   LibEvent_LIBRARIES
+#
+# Currently, this only supports libevent-svn (i.e. 1.5/2.0), so it's kind of useless for real use :)
+
+include (LibFindMacros)
+
+# include dir
+find_path (LibEvent_INCLUDE_DIR
+    NAMES "event2/event.h"
+    PATHS "$ENV{LIBEVENT_PREFIX}/include"
+)
+
+# library
+find_library (LibEvent_LIBRARY
+    NAMES "event"
+    PATHS "$ENV{LIBEVENT_PREFIX}/lib"
+)
+
+# set the external vars
+set (LibEvent_PROCESS_INCLUDES LibEvent_INCLUDE_DIR)
+set (LibEvent_PROCESS_LIBS LibEvent_LIBRARY)
+libfind_process (LibEvent)
